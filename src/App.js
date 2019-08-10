045f987a87aa2f983192bbfb001ac3355d4d7a18
Arif Ahmed <arif.ahmed.5.10.1995@gmail.com>
2019-08-10 22:01:57 +0530

    Improve JS implementation

--- src/App.js
+++ src/App.js
@@ -7,27 +7,70 @@ class App extends React.Component
 {
   constructor(props) {
     super(props);
-    this.state = { searchQueryGallery: "Test" };
+    this.state = {
+                  shows: [],
+                  filtered_shows: [],
+                };
+    this.filterShows = this.filterShows.bind(this);
+    this.sortShows = this.sortShows.bind(this);
+
+    let apiRequest = new XMLHttpRequest();
+
+    apiRequest.open('GET', 'https://api.tvmaze.com/schedule');
+    apiRequest.send();
+
+    apiRequest.onreadystatechange = () => {  
+        if(apiRequest.readyState === 4 && apiRequest.status === 200) {
+          this.setState({
+              shows: JSON.parse(apiRequest.responseText)
+            });
+            this.shows_real = JSON.parse(apiRequest.responseText);
+           let current_rating = 0;
+            for(let index = 0;index < this.state.shows.length;index++)
+            {
+              current_rating = this.state.shows[index].show.rating.average
+              if(current_rating === null)
+                this.state.shows[index].show.rating.average = 0;
+            }
+        }
+      };
   }
-  showCheck = () => {
-    let searchQuery = document.getElementById("showSearch");
-    this.setState({
-      searchQueryGallery: searchQuery
-    });
+
+  filterShows() {
+        let searchQueryGallery = document.getElementById("showSearch").value;
+
+        this.setState({filtered_shows : this.state.shows.filter(element => (
+            element.show.name.toLowerCase().search(searchQueryGallery.toLowerCase()) !== -1
+            || element.name.toLowerCase().search(searchQueryGallery.toLowerCase()) !== -1
+            || ((element.show.network === null) ? element.show.webChannel.name : element.show.network.name).toLowerCase().search(searchQueryGallery.toLowerCase()) !== -1
+            || ((element.number === null) ? "" : element.number.toString(10)).toLowerCase().search(searchQueryGallery.toLowerCase()) !== -1
+            || element.airdate.toLowerCase().search(searchQueryGallery.toLowerCase()) !== -1
+            || element.airtime.toLowerCase().search(searchQueryGallery.toLowerCase()) !== -1
+          ))});
+  }
+
+  sortShows() {
+    let fs = this.state.filtered_shows;
+    if(fs.length === 0)
+      fs = this.state.shows;
+    fs.sort(function(a, b) {return b.show.rating.average - a.show.rating.average;})
+    return fs;
   }
 
   render() {
-    if(this.state.searchQueryGallery === null)
-      this.state.searchQueryGallery = ""
     return (
       <div>
-        <strong>Search : </strong><input type="text"
-                                        name="showSearch"
-                                        id="showSearch"
-                                        onKeyUp={this.showCheck}></input>
-              <br></br>
-          <Gallery searchQueryGallery={this.state.searchQueryGallery.value}
-          />
+        <div align="middle">
+          <br></br>
+           <strong align="middle">Search : </strong><input type="text"
+                                            name="showSearch"
+                                            id="showSearch"
+                                            align="middle"
+                                            onKeyUp={this.filterShows}></input>
+            <br></br>
+            <br></br>
+        </div>
+          <Gallery shows={this.sortShows()} />
       </div>
     );
   }
--- src/Gallery.js
+++ src/Gallery.js
@@ -3,33 +3,14 @@ import React from 'react';
 class Gallery extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { currentIndex: null , shows: []};
-    this.imgUrls = [];
+    this.state = { currentIndex: null};
     this.renderImageContent = this.renderImageContent.bind(this);
+    this.thumbnailShowClicked = this.thumbnailShowClicked.bind(this);
   }
 
-  componentDidMount() {
-    let apiRequest = new XMLHttpRequest();
-
-    apiRequest.open('GET', 'https://api.tvmaze.com/schedule');
-    apiRequest.send();
-
-    apiRequest.onreadystatechange = () => {
-        if(apiRequest.readyState === 4 && apiRequest.status === 200)
-        {
-            let response = JSON.parse(apiRequest.responseText);
-            this.setState({
-              shows: response
-            });
-           let current_rating = 0;
-            for(let index = 0;index < this.state.shows.length;index++)
-            {
-              current_rating = this.state.shows[index].show.rating.average
-              if(current_rating === null)
-                this.state.shows[index].show.rating.average = 0;
-            }
-        }
-    };
+  componentDidUpdate(prevProps) {
+    if(this.props.shows !== prevProps.shows)
+      return null;
   }
 
   renderImageContent(show_info, index) {
@@ -37,12 +18,14 @@ class Gallery extends React.Component {
     let src = show_info.show.image.medium;
     let ep_name = show_info.name;
     let ep_number = show_info.number;
-    let network = show_info.show.network.name;
+
+    let network = (show_info.show.network === null) ? show_info.show.webChannel.name : show_info.show.network.name;
+
     let airdate = show_info.airdate;
     let airtime = show_info.airtime;
     return (
-      <div onClick={(e) => this.openModal(e, index)}>
-        <img src={src} key={src}/>
+      <div className="text-data-show">
+        <img src={src} key={src} onClick={(e) => this.thumbnailShowClicked(e, show_info.url)}/>
         <br></br>{show_name}<br></br>{ep_name}
         <br></br>Episode Number : {ep_number}
         <br></br>Airing on : {network}<br></br>{ep_name}
@@ -50,42 +33,18 @@ class Gallery extends React.Component {
       </div>
     ) 
   }
-  openModal(e, index) {
-    this.setState ({ currentIndex: index });
-    window.open(this.state.shows[index].url, '_blank').focus();
+
+  thumbnailShowClicked(e, url) {
+    window.open(url, '_blank').focus();
   }
-  closeModal(e) {
-    if (e != undefined) {
-      e.preventDefault();
-    }
-    this.setState ({ currentIndex: null });
-  }
-  
+
   render() {
-    let sq = this.props.searchQueryGallery;
-    if(sq == null)
-        sq = "";
-    let filtered_shows = [];
-    let total_shows = this.state.shows;
-    for(let index = 0;index < this.state.shows.length;index++)
+    if(this.props.shows.length !== 0)
     {
-        if(
-            total_shows[index].show.name.toLowerCase().search(sq.toLowerCase()) !== -1
-              || total_shows[index].name.toLowerCase().search(sq.toLowerCase()) !== -1
-              || total_shows[index].show.network.name.toLowerCase().search(sq.toLowerCase()) !== -1
-              || total_shows[index].number.toString(10).toLowerCase().search(sq.toLowerCase()) !== -1
-              || total_shows[index].airdate.toLowerCase().search(sq.toLowerCase()) !== -1
-              || total_shows[index].airtime.toLowerCase().search(sq.toLowerCase()) !== -1
-        )
-            filtered_shows.push(total_shows[index]);
-    }
-    if(filtered_shows.length !== 0)
-    {
-        filtered_shows.sort(function(a, b){return b.show.rating.average - a.show.rating.average;});
         return (
             <div className="gallery-container">
                 <div className="gallery-grid">
-                {filtered_shows.map(this.renderImageContent)}
+                {this.props.shows.map(this.renderImageContent)}
                 </div>
             </div>
         )
